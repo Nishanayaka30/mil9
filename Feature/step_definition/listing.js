d0843d1e40a3654f379715b9b663202e6250e09e
Nisha <nishanayaka@7edge.com>
2024-09-18 10:07:53 +0530

    2nd commit

--- Feature/step_definition/listing.js
+++ Feature/step_definition/listing.js
@@ -41,7 +41,7 @@ Before('@create_records', async function(){
 
 });
 
-Given('I\'m on the employee table page', async function(){
+Given('I\'m on the employee listing page', async function(){
     await driver.get('https://ninjatables.com/examples-of-data-table-design-on-website/');
     await new Promise(resolve => setTimeout(resolve,3000));
     driver.manage().window().maximize();
@@ -55,7 +55,7 @@ Then('I see empty page without any records', async function(){
 
 }); 
 
-When('I filter the list by status {string}', async function(data){
+When('I filter the list by designation {string}', async function(data){
     await driver.wait(until.elementLocated(By.css('[data-testid="dt-filter-0"]'))).sendKeys(Key.chord(CONTROL,'a',Key.DELETE),data); 
     await new Promise(resolve => setTimeout(resolve,3000));
 
@@ -81,68 +81,64 @@ Then('I see the message stated as {string}',async function(message){
         throw new Error("Failed");
     });
 
-When ('I search the table by fake name', async function(){
-   const name=faker.person.firstName();
+When ('I search the employee name by {string}', async function(name){
+
    await driver.wait(until.elementLocated(By.css('[data-testid="dt-search-0"]'))).sendKeys(Key.chord(CONTROL,'a',Key.DELETE),name);
+   const firstrowcell = await driver.wait(until.elementLocated(By.xpath('//*[@id=table]/tbody/tr[1]/td[2]')));  
+   const cellTexts = await Promise.all(firstrowcell.map(cell => cell.getText()));
+   const namefound = cellTexts.some(text => text.includes(name));
+
+   expect(namefound).to.be.false;
    await new Promise(resolve => setTimeout(resolve,3000));
 
 });
 
 
-When('I check the headers in the table', async function(){
-    this.head=await driver.wait(until.elementLocated(By.xpath('//*[@id=table]/thead/tr[1]')));  
-
-});
-
-Then("I should see the headers name", async function(){
+When('I see all the headers in the employee list', async function(){
+    const expectedHeaders = ['ID', 'Name', 'Age', 'Designation', 'Status']; 
+    this.header_row=await driver.wait(until.elementLocated(By.xpath('//*[@id=table]/thead/tr[1]')));  
     const headerCells = await header_row.findElements(By.css('th')); 
-    const headers = await Promise.all(headerCells.map(async cell => {
-    const text = await cell.getText();
-      return text.trim();
+    const headerTexts = await Promise.all(headerCells.map(cell => cell.getText()));
 
-}));
-
-const expectedHeaders = ['ID', 'Name', 'Age', 'Designation', 'Status']; 
-expect(headers).to.deep.equal(expectedHeaders);
+    for (const expectedHeader of expectedHeaders) {
+        expect(headerTexts).to.deep.equal(expectedHeader);
+    }
 
 });
 
-When('I filter the list by designation as {string}', async function(desig){
-    this.role = await driver.wait(until.elementLocated(By.css('[data-testid="dt-filter-0"]'))).sendKeys(Key.chord(CONTROL,'a',Key.DELETE),desig); 
+When('I filter the employee list by designation as {string}', async function(desig){
+    const role = await driver.wait(until.elementLocated(By.css('[data-testid="dt-filter-0"]'))).sendKeys(Key.chord(CONTROL,'a',Key.DELETE),desig); 
     await new Promise(resolve => setTimeout(resolve,3000));
-});
-
-
-Then('I see the filtered list',async function(){
     let rows = await driver.wait(until.elementsLocated(By.xpath('//*[@id=table]/tbody/tr')));
     for (let row of rows) {
       let column_data = await row.findElement(By.xpath('//*[@id=table]/tbody/tr/td[4]'));
       let row_Text = await column_data.getText();
       expect(row_Text).to.equal(role);
-
     }
-
 });
 
-When('I search the table by name as {string}', async function(name){
+
+
+When('I search the employee list by name', async function(){
+   this.name=person1;
    this.actual_search=await driver.wait(until.elementLocated(By.css('[data-testid="dt-search-0"]'))).sendKeys(Key.chord(CONTROL,'a',Key.DELETE),name);
     await new Promise(resolve => setTimeout(resolve,3000));
+});
+
+Then('I should see the searched name in the employee list',async function(){ 
+    const firstrowcell = await driver.wait(until.elementLocated(By.xpath('//*[@id=table]/tbody/tr[1]/td[2]')));  
+   const cellTexts = await Promise.all(firstrowcell.map(cell => cell.getText()));
+   const namefound = cellTexts.some(text => text.includes(this.name));
+
+   expect(namefound).to.be.true;
+   await new Promise(resolve => setTimeout(resolve,3000));
 
 });
 
 
-
-Then('I see the row of the employee data', async function(){
-    let name_element=await driver.wait(until.elementLocated(By.xpath('//*[@id=example]/tbody/tr/td[2]')))
-    let name_text=await name_element.getText();
-    expect(name_text).to.equal(actual_search);
-
-});
-
-
-When('I sort the table by name', async function(){
+When('I click on sort button based on employee name', async function(){
     await driver.wait(until.elementLocated(By.xpath('//*[@id=table]/thead/tr[1]/th[2]/span[@class="dt-column-title"][1]'))).click(); //same classname for all the headers so it is span[@cls=''][1] 
-
+    await new Promise(resolve => setTimeout(resolve,3000));
 });
 
 Then('I see the list of employee data in ascending order',async function(){
@@ -152,12 +148,24 @@ Then('I see the list of employee data in ascending order',async function(){
     expect(names).to.deep.equal(sortednames);
 });
 
-When('I click on the next page',async function(button){
-    await driver.wait(until.elementLocated(By.css('[data-testid="next-page"]'))).click(button);
-
+Then('I see the list of employee data in decending order',async function(){
+    const all_elements = await driver.findElements(By.css('.dt-column-title2'));
+    const names=await Promise.all(all_elements.map(async element => await element.getText()));
+    const sortednames = [...names].sort().reverse();
+    expect(names).to.deep.equal(sortednames);
 });
 
-Then('I should see the next rows of table', async function(){
+When('I navigate to the next page',async function(button){
+    await driver.wait(until.elementLocated(By.css('[data-testid="next-page"]'))).click(button);
+    await new Promise(resolve => setTimeout(resolve,3000));
+});
+
+Then('I should see the next page number highlighted and filtered list of employees', async function(){
+    let page_number = 2;
+    const nextpagebutton = await driver.findElement(By.xpath(`//button[@class='page-number' and text()='${page_number}']`));
+    const pagecolor = await nextpagebutton.getCssValue('background-color');
+    const bluecolor= 'rgba(0, 0, 255, 1)';
+    expect(pagecolor).to.equal(bluecolor);
 
     await driver.wait(until.elementLocated(By.css('#table')), 5000);
     await new Promise(resolve => setTimeout(resolve,3000));
@@ -167,10 +175,15 @@ Then('I should see the next rows of table', async function(){
 When('I click on the previous page', async function(button){
 
     await driver.wait(until.elementLocated(By.css('[data-testid="previous-page"]'))).click(button);
-
+    await new Promise(resolve => setTimeout(resolve,3000));
 });
 
-Then('I see the previous rows of table', async function(){
+Then('I should see the previous page number highlighted and filtered list of employees ', async function(){
+    let page_number = 1;
+    const nextpagebutton = await driver.findElement(By.xpath(`//button[@class='page-number' and text()='${page_number}']`));
+    const pagecolor = await nextpagebutton.getCssValue('background-color');
+    const bluecolor= 'rgba(0, 0, 255, 1)';
+    expect(pagecolor).to.equal(bluecolor);
 
     await driver.wait(until.elementLocated(By.css('#table')), 5000);
     await new Promise(resolve => setTimeout(resolve,3000));
@@ -197,18 +210,6 @@ Then('I see the rows which has the Designation', async function(){
 });
 
 
-When('I go to the next page', async function(){
-    await driver.wait(until.elementLocated(By.css('[data-testid="next-page"]'))).click(button);
-    await driver.wait(until.elementLocated(By.css('#table')), 5000);
-});
-
-
-When('I go to the previous page', async function(){
-
-    await driver.wait(until.elementLocated(By.css('[data-testid="previous-page"]'))).click(button);
-    await driver.wait(until.elementLocated(By.css('#table')), 5000);
-
-});
 
 AfterAll(async function(){
 
